fd64bc716365203a3b7ffbbf3ea800aadffb1ab6
Juan Miguel De La Puente Garcia <99931391+ImJuanPt@users.noreply.github.com>
2024-09-06 18:55:39 -0500

    Add files via upload

---
+++ EjemploLipeRmiClienteImc/src/juanpuente/seminario/imc/rmi/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package juanpuente.seminario.imc.rmi;
+
+import juanpuente.seminario.imc.rmi.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author Juan Miguel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
